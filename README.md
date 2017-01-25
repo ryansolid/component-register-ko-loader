3ec65ea4daab4fabdc798e9449cf664401149336
Ryan Carniato <ryansolid@gmail.com>
2017-01-25 02:15:50 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Introduction
+============
+
+Component Register KO Loader is a loader for webpack to convert react-like syntax bindings to KO data-bind attribute.
+
---
+++ index.js
@@ -0,0 +1,5 @@
+var preprocessor = require('component-register-ko/lib/preprocessor/index.js')
+
+module.exports = function (content) {
+  return preprocessor(content)
+}
